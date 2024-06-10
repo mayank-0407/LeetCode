19784a1d222be3a8e3a318b6ac7651e1c2f555bc
Mayank Aggarwal <97796657+mayank-0407@users.noreply.github.com>
2024-06-10 20:47:48 +0530

    Time: 5 ms (31.17%) | Memory: 10 MB (96.49%) - LeetSync

--- 1137-height-checker/height-checker.cpp
+++ 1137-height-checker/height-checker.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     int heightChecker(vector<int>& heights) {
         vector<char> sorted(heights.begin(), heights.end());
-        // counting sort
         char freq[101]={0};
         for(char x: sorted) freq[x]++;
         int cnt=0;
@@ -12,7 +11,6 @@ public:
             copy(begin(f_x), end(f_x), sorted.begin()+cnt);
             cnt+=f;
         }
-        //end of count sort
 
         int n=heights.size(), ans=0;
         for(int i=0; i<n; i++)

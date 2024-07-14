1626f26bfc09ba9c79e593ced465e06ec62ee492
Mayank Aggarwal <97796657+mayank-0407@users.noreply.github.com>
2024-07-14 18:27:11 +0530

    Time: 21 ms (17.51%) | Memory: 19.6 MB (69.26%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -15,20 +15,22 @@ public:
     int change(int amount, vector<int>& arr) {
         int n=arr.size();
         // vector<vector<int>> dp(n,vector<int>(target+1,-1)); // for memoize
-        vector<vector<int>> dp(n,vector<int>(amount+1,0));
+        vector<int> prev(amount+1,0);
         for(int t=0;t<=amount;t++)
-            if(t%arr[0]==0) dp[0][t]=1;
+            if(t%arr[0]==0) prev[t]=1;
 
         for(int idx=1;idx<n;idx++){
+            vector<int> curr(amount+1,0);
             for(int target=0;target<=amount;target++){
-                int nottake=dp[idx-1][target];
+                int nottake=prev[target];
                 int take =0;
                 if(target>=arr[idx])
-                    take=dp[idx][target-arr[idx]];
-                dp[idx][target]=take+nottake;
+                    take=curr[target-arr[idx]];
+                curr[target]=take+nottake;
             }
+            prev=curr;
         }
 
-        return dp[n-1][amount];
+        return prev[amount];
     }
 };

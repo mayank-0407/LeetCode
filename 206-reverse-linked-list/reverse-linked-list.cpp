3d2042c267e4f3c7fd658838399696987f112795
Mayank Aggarwal <97796657+mayank-0407@users.noreply.github.com>
2024-08-08 18:54:37 +0530

    Time: 4 ms (66.46%) | Memory: 13.3 MB (8.90%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.cpp
+++ 206-reverse-linked-list/reverse-linked-list.cpp
@@ -11,11 +11,14 @@
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-        if(head == NULL || head->next==NULL) return head;
-        ListNode *newhead=reverseList(head->next);
-        ListNode *front=head->next;
-        front->next=head;
-        head->next=NULL;
-        return newhead;
+        if(head == NULL || head->next == NULL) return head;
+
+        ListNode* newHead = reverseList(head->next);
+
+        ListNode* front = head->next;
+        front->next = head;
+        head->next = NULL;
+
+        return newHead;
     }
 };

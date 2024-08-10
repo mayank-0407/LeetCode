b33881a7f621815c91cc8cbdf059ffff11891c58
Mayank Aggarwal <97796657+mayank-0407@users.noreply.github.com>
2024-08-10 20:47:48 +0530

    Time: 64 ms (59.82%) | Memory: 29.4 MB (34.75%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,32 +1,25 @@
 class Solution {
 public:
-    vector<vector<int>> threeSum(vector<int>& arr) {
-        int n=arr.size();
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
         vector<vector<int>> ans;
-        sort(arr.begin(), arr.end());
-        for (int i = 0; i < n; i++) {
-            //remove duplicates:
-            if (i != 0 && arr[i] == arr[i - 1]) continue;
+        for(int i = 0; i < n; i++){
+            if(i != 0 && nums[i] == nums[i-1]) continue;
 
-            //moving 2 pointers:
-            int j = i + 1;
-            int k = n - 1;
-            while (j < k) {
-                int sum = arr[i] + arr[j] + arr[k];
-                if (sum < 0) {
-                    j++;
-                }
-                else if (sum > 0) {
-                    k--;
-                }
-                else {
-                    vector<int> temp = {arr[i], arr[j], arr[k]};
+            int j = i+1, k = n-1;
+            while(j<k){
+                int sum = nums[i] + nums[j] + nums[k];
+                if(sum<0) j++;
+                else if(sum>0) k--;
+                else{
+                    vector<int> temp = {nums[i], nums[j], nums[k]};
                     ans.push_back(temp);
                     j++;
                     k--;
-                    //skip the duplicates:
-                    while (j < k && arr[j] == arr[j - 1]) j++;
-                    while (j < k && arr[k] == arr[k + 1]) k--;
+
+                    while(j<k && nums[j] == nums[j-1]) j++;
+                    while(j<k && nums[k] == nums[k+1]) k--;
                 }
             }
         }
